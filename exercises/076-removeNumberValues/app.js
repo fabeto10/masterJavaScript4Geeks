9950579d6b6f49c6159868e3b0a6e02222ab650a
fabeto10 <f0414475@gmail.com>
2022-10-23 14:22:55 -0400

    haciendo ejercicos

--- exercises/076-removeNumberValues/app.js
+++ exercises/076-removeNumberValues/app.js
@@ -1,12 +1,15 @@
-let obj = {
+let objective = {
     a: 2,
     b: 'remaining',
     c: 4
 };
 function removeNumberValues(obj) {
     // your code here
-    
-}
-
-removeNumberValues(obj);
-console.log(obj); // --> { b: 'remaining' }
+    const globalVar = Object.assign({}, obj);
+    console.log(globalVar)
+    let aux = [];
+    let change = Object.values(obj);
+    let result = change.filter( param => Number(param) !== param ? aux.push(param) : false );
+    return result;
+};
+console.log(removeNumberValues(objective)); // --> { b: 'remaining' }
